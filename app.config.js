ea37cfbcffc421b7b75d859e3a490f93d623971a
marcosvaleriofilho <marcosvaleriofilho@outlook.com>
2025-06-09 16:53:30 -0300

    feat: add configuration for EAS and update app settings

--- app.config.js
+++ app.config.js
@@ -1,5 +1,31 @@
 export default ({ config }) => ({
   ...config,
+  name: "adotai-frontend",
+  slug: "adotai-frontend",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/adotai-logo-png.png",
+  userInterfaceStyle: "light",
+  newArchEnabled: true,
+  splash: {
+    image: "./assets/images/adotai-logo-png.png",
+    resizeMode: "cover",
+    backgroundColor: "#8A1732"
+  },
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.adotai.mobile"
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adotai-logo-png.png",
+      backgroundColor: "#8A1732"
+    },
+    package: "com.adotai.mobile"
+  },
+  web: {
+    favicon: "./assets/favicon.png"
+  },
   extra: {
     firebaseApiKey: process.env.FIREBASE_API_KEY,
     firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -8,6 +34,9 @@ export default ({ config }) => ({
     firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     firebaseAppId: process.env.FIREBASE_APP_ID,
     firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
-    USER_ROUTE: process.env.USER_ROUTE, // <-- Adicione aqui
-  },
+    USER_ROUTE: process.env.USER_ROUTE,
+    eas: {
+      projectId: "479ba39c-112e-4f95-b95e-3148c85c743c" // <-- Isso é o que o EAS precisa!
+    }
+  }
 });
